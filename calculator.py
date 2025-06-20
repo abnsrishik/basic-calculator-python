6386714ca79a64552e049c596cd41608886a98ba
A.B.N.S Rishik <rishikcr72401@gmail.com>
2025-06-20 08:23:50 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,18 @@
-def add(a, b): return a + b
-def subtract(a, b): return a - b
-def multiply(a, b): return a * b
-def divide(a, b): return a / b if b != 0 else 'Undefined'
-
 print("Simple Calculator")
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 op = input("Enter operation (+, -, *, /): ")
 
-if op == '+': print("Result:", add(a, b))
-elif op == '-': print("Result:", subtract(a, b))
-elif op == '*': print("Result:", multiply(a, b))
-elif op == '/': print("Result:", divide(a, b))
-else: print("Invalid operation")
+if op == '+': 
+  print("Result:", a+b)
+elif op == '-': 
+  print("Result:", a-b)
+elif op == '*': 
+  print("Result:", a*b)
+elif op == '/':
+  if b != 0:
+      print('Result: ', a/b)
+  else:
+      print("Invalid operation")
+else:
+    print("Invalid operation")
