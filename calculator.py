2b71b92943176894eaf206aaf2fc75004949a8e6
A.B.N.S Rishik <rishikcr72401@gmail.com>
2025-06-20 08:32:46 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,18 +1,25 @@
-print("Simple Calculator")
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-op = input("Enter operation (+, -, *, /): ")
-
-if op == '+': 
-  print("Result:", a+b)
-elif op == '-': 
-  print("Result:", a-b)
-elif op == '*': 
-  print("Result:", a*b)
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+print('Simple Calculator')
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+op = input('Enter Operation (+,-,*,/): ')
+if op=='+':
+    print('Result: ', add(a,b))
+elif op == '-':
+    print('Result: ', sub(a,b))
+elif op == '*':
+    print('Result: ', mul(a,b))
 elif op == '/':
-  if b != 0:
-      print('Result: ', a/b)
-  else:
-      print("Invalid operation")
+    if b!=0:
+        print('Result: ', div(a,b))
+    else:
+        print('Invalid operation')
 else:
-    print("Invalid operation")
+    print('Invalid operation')
