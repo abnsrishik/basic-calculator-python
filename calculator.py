ae40180e0966ed477c851ec25cdc64de646d95c4
A.B.N.S Rishik <rishikcr72401@gmail.com>
2025-06-20 08:35:23 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+## defining functions
 def add(a,b):
     return a+b
 def sub(a,b):
@@ -6,10 +7,14 @@ def mul(a,b):
     return a*b
 def div(a,b):
     return a/b
+    
 print('Simple Calculator')
+# assigning Variables
 a = int(input('Enter first number: '))
 b = int(input('Enter second number: '))
 op = input('Enter Operation (+,-,*,/): ')
+
+# using above functions with if/else statements
 if op=='+':
     print('Result: ', add(a,b))
 elif op == '-':
@@ -17,6 +22,7 @@ elif op == '-':
 elif op == '*':
     print('Result: ', mul(a,b))
 elif op == '/':
+    # give result only if b not equal to 0
     if b!=0:
         print('Result: ', div(a,b))
     else:
